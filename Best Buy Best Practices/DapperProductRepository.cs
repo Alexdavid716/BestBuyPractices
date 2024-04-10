0d2b0ab6b678dc6d95725ad9a54ccaa6778465ad
Alex David <alexdavid716@gmail.com>
2024-04-10 10:28:50 -0400

    best buy practices

---
+++ Best Buy Best Practices/DapperProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using Dapper;
+
+namespace Best_Buy_Best_Practices
+{
+	public class DapperProductRepository : IProductRepository 
+	{
+        private readonly IDbConnection _connection;
+
+		public DapperProductRepository(IDbConnection connection )
+		{
+            _connection = connection;
+		}
+
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _connection.Execute("INSERT INTO products(Name, Price, CategoryID)" +
+                "VALUES (@name,@price, @categoryID);",
+                new { name = name, price = price, categoryID });
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM products;");
+        }
+    }
+}
+
---
+++ Best Buy Best Practices/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Best_Buy_Best_Practices
+{
+	public interface IProductRepository
+	{
+		public IEnumerable<Product> GetAllProducts();
+
+		public void CreateProduct(string name, double price, int categoryID);
+	}
+}
+
